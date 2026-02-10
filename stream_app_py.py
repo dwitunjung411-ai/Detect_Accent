a0b2c47c8dbf87004f78ee1000cf531af21627a8
dwitunjung411-ai <dwitunjung411@gmail.com>
2026-02-10 22:58:08 +0700

    back to pertama

--- stream_app_py.py
+++ stream_app_py.py
@@ -12,7 +12,7 @@ import os
 def load_accent_model():
     import tensorflow as tf
     
-    model_path = "model_embedding_aksen.keras"
+    model_path = "model_aksen.keras"
     
     # Cek file ada atau tidak
     if not os.path.exists(model_path):
